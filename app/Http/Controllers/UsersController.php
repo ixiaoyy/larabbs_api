5ef660eba2c68ab65c12502a1154679feb665b4a
xiaoyz <phpxiaoyz@163.com>
2020-03-31 12:32:59 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,6 +14,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     // 创建一个 show 方法展示用户个人页面
     public function show(User $user)
     {
@@ -27,6 +32,7 @@ class UsersController extends Controller
       */
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -37,6 +43,7 @@ class UsersController extends Controller
       */
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
