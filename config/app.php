b1251597a78e0bf155eef08df811b0c5171b1e8e
xiaoyz <phpxiaoyz@163.com>
2020-04-17 12:41:48 +0000

    短信调试

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 短信
+        App\Providers\EasySmsServiceProvider::class,
     ],
 
     /*
